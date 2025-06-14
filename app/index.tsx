0d93a1444cb58461399f03d530d0c4a0522e1572
llite22 <llite2@yandex.ru>
2025-06-14 19:06:19 +0300

    Page login + component Image

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,39 @@
-import { Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Image style={styles.logo} resizeMode="contain" source={require("../assets/logo.png")} />
+        <View style={styles.form}>
+          <TextInput style={styles.input} placeholder="Email" />
+          <TextInput style={styles.input} placeholder="Password" />
+          <Button title="Войти" />
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+    padding: 55,
+    backgroundColor: "#16171D",
+  },
+  content: {
+    alignItems: "center",
+    gap: 50,
+  },
+  logo: {
+    width: 220,
+  },
+  form: {
+    alignSelf: "stretch",
+    gap: 16,
+  },
+  input: {
+    backgroundColor: "#2e2d3d",
+  },
+});
