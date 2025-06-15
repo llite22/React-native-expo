a01db67c169fe253e724ad5ecf1a399d341c8852
llite22 <llite2@yandex.ru>
2025-06-15 20:26:15 +0300

    prettier + eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,63 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const reactPlugin = require('eslint-plugin-react');
+const reactNativePlugin = require('eslint-plugin-react-native');
+const prettierPlugin = require('eslint-plugin-prettier');
 const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+	expoConfig,
+	{
+		ignores: ['dist/**'],
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				project: './tsconfig.json',
+				ecmaVersion: 2022,
+				sourceType: 'module',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tsPlugin,
+			react: reactPlugin,
+			'react-native': reactNativePlugin,
+			prettier: prettierPlugin,
+		},
+		rules: {
+			...tsPlugin.configs.recommended.rules,
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+			],
+			'react-native/no-unused-styles': 2,
+			'react-native/split-platform-components': 2,
+			'react-native/no-inline-styles': 2,
+			'react-native/no-color-literals': 2,
+			// 'react-native/no-raw-text': 2,
+			'react-native/no-single-element-style-arrays': 2,
+			'react-hooks/exhaustive-deps': 'off',
+			'prettier/prettier': [
+				'error',
+				{
+					singleQuote: true,
+					useTabs: true,
+					semi: true,
+					trailingComma: 'all',
+					bracketSpacing: true,
+					printWidth: 100,
+					endOfLine: 'auto',
+				},
+			],
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/interface-name-prefix': 'off',
+			'@typescript-eslint/ban-types': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+		},
+	},
 ]);
